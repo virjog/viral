41a58030632a42ebe6ac054eaff997b549d9eee1
Viral Jogani <viraljogani@gmail.com>
2015-05-18 15:10:02 -0400

    new site

--- js/particles.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-      $('#particles').particleground({
-        dotColor: '#52B3D9',
-        lineColor: '#52B3D9'
-      });
-      $('.container').css({
-        'margin-top': -($('.container').height() / 0.75)
-      });
-    });
